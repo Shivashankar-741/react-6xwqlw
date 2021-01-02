8d6ede7f5a12b2e9a12a8223b098a4d302a8465b
Shivashankar-741 <shiva1894300@gmail.com>
2021-01-02 16:32:12 +0530

    1 file modified

--- src/UseRef.js
+++ src/UseRef.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 const UseRef = () => {
+  // React useref allow us to store the data without re-reendering the component
+  const storeRef = React.useRef(0);
+
+  console.log(storeRef.current);
+
+  React.useEffect(() => {
+    console.log("rendering the useEffect hooks");
+    storeRef.current = storeRef.current + 1;
+  });
   return (
     <>
       <h1>UseRef</h1>
+      <button onClick={()=>console.log("adsfsa")}>click</button>
+      {storeRef.current}
     </>
   );
 };
