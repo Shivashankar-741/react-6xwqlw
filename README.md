535a066d8b13c33029de9ff530315ae56bf85402
Shivashankar-741 <shiva1894300@gmail.com>
2021-01-02 08:11:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-6xwqlw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-6xwqlw)
---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./style.css";
+
+export class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      value: 1
+    };
+    console.log("constructor");
+  }
+
+  componentWillMount() {
+    console.log("component will mount");
+  }
+
+  componentDidMount() {
+    console.log("Component did mount");
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log("component will receive props", nextProps);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("Should component Update", nextProps, nextState);
+    return true;
+    // return false;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log("component will update", nextProps, nextState);
+  }
+
+  componentDidUpdate(prevProps, nextState) {
+    console.log("component did update", prevProps, nextState);
+  }
+
+  componentWillUnmount() {
+    console.log("component will unmount");
+  }
+
+  render() {
+    console.log("rendering method;");
+    return (
+      <>
+        <h1>App react Component {this.state.value}</h1>
+        <button
+          onClick={() =>
+            this.setState({
+              value: this.state.value + 1
+            })
+          }
+        >
+          click me
+        </button>
+      </>
+    );
+  }
+}
+
+export default App;
